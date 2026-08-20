dc903b1d2e6b07340037e9006102f208f47cd77c
Princessa <sofiarey524@gmail.com>
2026-08-20 19:13:28 +0700

    Add string manipulation programs

---
+++ initials.py
@@ -0,0 +1,5 @@
+name = input("Введите свое имя: ")
+surname = input("Введите свою фамилию: ")
+otchestvo = input("Введите свое отчество: ")
+
+print(f"{surname} {name[0]}. {otchestvo[0]}.")
---
+++ name_case.py
@@ -0,0 +1,5 @@
+name = input("Введите своё имя(с заглавной буквы): ")
+surname = input("Введите свою фамилию(с заглавной буквы): ")
+
+print(name.upper(), surname.upper())
+print(name.lower(), surname.lower())
