7c2b19d09d385ce22c9798b1b81e2f5103e47c06
Princessa <sofiarey524@gmail.com>
2026-09-03 13:15:14 +0700

    Add modules practice: math and random

---
+++ modules_practice.py
@@ -0,0 +1,9 @@
+import math
+import random
+
+print(math.sqrt(81))    # Корень числа 81
+print(math.pi)    # Число Пи
+print(math.sin(math.radians(30)))   # Синус 30 градусов
+
+for _ in range(5):  # Рандомные 5 чисел от 1 до 100
+    print(random.randint(1, 100))
