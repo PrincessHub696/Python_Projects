6ec5b27a2a55b508cd304486b76d3c1ceaa5dec1
Princessa <sofiarey524@gmail.com>
2026-09-18 11:32:40 +0300

    Remove duplicate files from root

--- text_analyzer.py
+++
@@ -1,9 +0,0 @@
-def analyze_text(text):
-    words = len(text.split())
-    letters = len(text.replace(" ", ""))
-    return words, letters
-
-text = input("Введите текст: ")
-words, letters = analyze_text(text)
-print(f"Количество слов: {words}")
-print(f"Количество символов (без пробелов): {letters}")
